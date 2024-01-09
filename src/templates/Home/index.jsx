838110758ae8de47bf9ac782adcdc4c0d770ca42
Vinicius Souza <vinicius.msouza07@gmail.com>
2024-01-09 19:32:14 -0300

    primeiro commit preparação

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { Component } from 'react';
+
+
+export class Home extends Component {
+
+
+  render () {
+    
+    return(
+      <div>Olá mundo</div>
+    )
+  }
+}
+
